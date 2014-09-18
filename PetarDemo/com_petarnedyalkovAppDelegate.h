144468ef10b66d2ad7d510c0116214959453d423
Petar Nedyalkov <pnedyalkov@Petar-Nedyalkovs-MacBook-Pro.local>
2014-09-17 22:06:21 -0700

    Initial Commit

---
+++ PetarDemo/com_petarnedyalkovAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_petarnedyalkovAppDelegate.h
+//  PetarDemo
+//
+//  Created by Petar Nedyalkov on 9/17/14.
+//  Copyright (c) 2014 Petar Nedyalkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_petarnedyalkovAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PetarDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PetarDemo
+//
+//  Created by Petar Nedyalkov on 9/17/14.
+//  Copyright (c) 2014 Petar Nedyalkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_petarnedyalkovAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_petarnedyalkovAppDelegate class]));
+    }
+}
